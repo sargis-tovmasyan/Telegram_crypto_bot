b0917108003e4678633504d56c6efbc3a2e8d78d
Saqo1995s <35834865+Saqo1995s@users.noreply.github.com>
2022-08-05 21:30:05 +0400

    Update variables.py

--- variables.py
+++ variables.py
@@ -5,8 +5,8 @@ CHAT_GROUP 	= '-1001441068751'
 CHAT_BOT 	= '536992397'
 #================================================================#
 #================= responce messages of commands ================#
-start_msg 	= 'Welcome' 								 		#/start
-help_msg 	= 'This is a bot assistant for my afk time'  		#/help
+start_msg 	= 'Welcome' #/start
+help_msg 	= 'The bot can help you with tracking the crypto-currencies'  #/help
 about_msg 	= 'Bot for telegram created by Sargis Tovmasyan'	#/about
 #================================================================#
 success_msg 	= 'Message sents tov bots chat!'
